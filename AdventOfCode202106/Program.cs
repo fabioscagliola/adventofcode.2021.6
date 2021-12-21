b676877b20096d60898eee67af86848eb8030900
Fabio Scagliola <scagliola@gmail.com>
2021-12-21 13:55:45 +0100

    The first half of this puzzle is complete!

---
+++ AdventOfCode202106/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace com.fabioscagliola.AdventOfCode202106
+{
+    class Program
+    {
+        class Lanternfish
+        {
+            public event EventHandler LanternfishBorn;
+
+            protected int internalTimer;
+
+            public Lanternfish(int internalTimer)
+            {
+                this.internalTimer = internalTimer;
+            }
+
+            public void LiveOneDay()
+            {
+                internalTimer--;
+
+                if (internalTimer == -1)
+                {
+                    internalTimer = 6;
+                    LanternfishBorn?.Invoke(this, new EventArgs());
+                }
+            }
+        }
+
+        class LanternfishList : List<Lanternfish>
+        {
+            protected List<Lanternfish> babyLanternfishList;
+
+            public LanternfishList(int[] initialStateList)
+            {
+                foreach (int initialState in initialStateList)
+                {
+                    Lanternfish lanternfish = new Lanternfish(initialState);
+                    lanternfish.LanternfishBorn += LanternfishBornEventListener;
+                    Add(lanternfish);
+                }
+            }
+
+            protected void LanternfishBornEventListener(object sender, EventArgs e)
+            {
+                Lanternfish lanternfish = new Lanternfish(8);
+                lanternfish.LanternfishBorn += LanternfishBornEventListener;
+                babyLanternfishList.Add(lanternfish);
+            }
+
+            public int CountAfterDays(int days)
+            {
+                for (int i = 0; i < days; i++)
+                {
+                    babyLanternfishList = new List<Lanternfish>();
+
+                    foreach (Lanternfish lanternfish in this)
+                    {
+                        lanternfish.LiveOneDay();
+                    }
+
+                    this.AddRange(babyLanternfishList);
+                }
+
+                return Count;
+            }
+        }
+
+        static void Main()
+        {
+            int[] initialStateList = File.ReadAllText("Input1.txt")
+                .Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .ToList().Select(x => int.Parse(x)).ToArray();
+
+            LanternfishList lanternfishList = new LanternfishList(initialStateList);
+
+            Console.WriteLine($"After 80 days there would be {lanternfishList.CountAfterDays(80)} lanternfish");
+        }
+
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# adventofcode.2021.6
-Advent of Code 2021 – Day 6: Lanternfish 
+# Advent of Code 2021 – Day 6: Lanternfish
+
+This is how I solved [Advent of Code 2021 – Day 6: Lanternfish](https://adventofcode.com/2021/day/6) 
+
+The solution consists of a console application written in C# based on the Microsoft .NET Framework 4.6 
+
